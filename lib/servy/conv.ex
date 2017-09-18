6e830ee458b44a0888154a1e60718718afabd226
cateland <catelandaxel@gmail.com>
2017-09-18 23:41:33 +0200

    step 12

---
+++ lib/servy/conv.ex
@@ -0,0 +1,18 @@
+defmodule Servy.Conv do
+    defstruct method: "", path: "", status: nil, resp_body: ""
+    
+    def full_status(%Servy.Conv{status: status}) do
+        "#{status} #{status_reason(status)}"
+    end
+
+    defp status_reason(code) do
+        %{
+            200 => "OK",
+            201 => "Created",
+            401 => "Unhauthorized",
+            403 => "Forbidden",
+            404 => "Not Found",
+            500 => "Internal Server Error",
+        }[code]
+    end
+end
--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -1,4 +1,7 @@
 defmodule Servy.Parser do
+
+    alias Servy.Conv
+
     def parse(request) do
         [method, path, _] =
             request 
@@ -6,11 +9,9 @@ defmodule Servy.Parser do
             |> List.first
             |> String.split(" ")
 
-        %{
+        %Conv{
             method: method,
-            path: path,
-            status: nil,
-            resp_body: ""
+            path: path
         }
     end
 end
